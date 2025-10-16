091dbcf1c5f0d658f3fafb90e2d86741407664dd
Evan Adam <eadam@ensiie.eu>
2025-10-16 11:31:42 +0200

    add rust gitignore

--- src/grpc/client.rs
+++ src/grpc/client.rs
@@ -2,7 +2,7 @@ use grpc_ease::reflection::ReflectionClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let endpoint = "http://127.0.0.1:5
+    let endpoint = "http://127.0.0.1:50051";
 
     let mut reflection_client = ReflectionClient::new(endpoint.to_string()).await?;
     let services = reflection_client.list_services().await?;
